679815f2a7a8ccd384dbc13e61f8c3c2dda59d58
Gregory Gross <greg@gtgross.com>
2009-10-30 12:40:55 -0400

    added regular expression pattern matching!

---
+++ regex/regex.rb
@@ -0,0 +1,72 @@
+#!/usr/bin/env ruby
+
+require 'set'
+
+require 'nfa'
+require 'parser'
+
+module Regex
+
+    class Regex
+
+        class << self
+            def match(pattern, str)
+                nfa = NFA.construct(Parser.parse_tree(pattern))
+                matcher = Matcher.new(nfa)
+                matcher.match(str)
+            end
+        end
+
+        class Matcher
+            attr_accessor :states, :pos, :nfa
+            
+            def initialize(nfa)
+                @pos = 0
+                @states = Set.new
+                @nfa = nfa
+            end
+
+            def match(str)
+                @states << @nfa.start
+                move_epsilon
+                str.each_char do |char|
+                    move(char)
+                    move_epsilon
+                end
+                matched = false
+                @states.each do |state|
+                    matched = true if @nfa.accept == state
+                end
+                matched
+            end
+
+            def move(char)
+                new_states = Set.new
+                @states.each do |start|
+                    finish = @nfa.transitions[[start, char]]
+                    new_states = new_states | finish if finish
+                end
+                @states = new_states
+            end
+
+            def move_epsilon
+                new_states = Set.new
+                while true do
+                    snapshot = new_states
+                    @states.each do |start|
+                        finish = @nfa.transitions[[start, nil]]
+                        new_states = new_states | finish if finish
+                    end
+                    @states = new_states
+                    break if snapshot.size == new_states.size
+                end
+            end
+        end
+
+    end
+
+end
+
+if __FILE__ == $0
+    puts Regex::Regex.match(ARGV[0], ARGV[1])
+end
